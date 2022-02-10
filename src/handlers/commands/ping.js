43b809c60ac83c8bc3f99f5fffca9153c4c6465b
Tymianek <wilku.tymon@gmail.com>
2022-02-10 10:44:17 +0100

    Add better ping command

--- src/handlers/commands/ping.js
+++ src/handlers/commands/ping.js
@@ -1,11 +1,35 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong 🏓'),
+     data: new SlashCommandBuilder()
+          .setName("ping")
+          .setDescription("Replies with Pong 🏓"),
 
-    async execute(interaction) {
-        await interaction.reply({ content: 'Pong 🏓', ephemeral: true});
-    },
+     async execute(interaction) {
+          let uptime = Math.floor(Client.uptime / 1000 / 60 / 60);
+          let sufix = "h";
+          if (uptime === 0) {
+               uptime = Math.floor(Client.uptime / 1000 / 60);
+               sufix = "min";
+          }
+          if (uptime === 0) {
+               uptime = Math.floor(Client.uptime / 1000);
+               sufix = "sec";
+          }
+          const embed = new MessageEmbed()
+               .setTitle("Ping command")
+               .setTimestamp()
+               .addField("API latency", `${Math.round(Client.ws.ping)}ms`)
+               .addField(
+                    "Bot latency",
+                    `${Math.round(Date.now() - interaction.createdTimestamp)}ms`
+               )
+               .addField("Uptime", `${uptime}${sufix}`)
+               .setFooter("/ping")
+               .setColor("RANDOM");
+          await interaction.reply({
+               embeds: [embed],
+               ephemeral: true,
+          });
+     },
 };
