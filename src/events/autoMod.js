e00d29466530619a04269e29b44449cf7b4096ef
Tymianek <wilku.tymon@gmail.com>
2022-04-06 21:30:53 +0200

    ✨Remove useless comments

--- src/events/autoMod.js
+++ src/events/autoMod.js
@@ -4,11 +4,10 @@ class CacheManager {
      constructor(val) {
           this.cache = String(val).split(/[\n\r]/);
           this.cache = this.cache.filter((url) => url.length > 0);
-          // change it to map as TOT said
           this.cache = new Map(this.cache.map((url) => [url, true]));
      }
      has(val) {
-          return this.cache.has(val); // use Map.prototype.has function
+          return this.cache.has(val);
      }
 }
 
@@ -18,7 +17,7 @@ module.exports = {
      name: "messageCreate",
      once: false,
      execute(msg) {
-          if (cache.has(msg.content)) { // brackets for lost
+          if (cache.has(msg.content)) {
                msg.delete();
           }
      },
